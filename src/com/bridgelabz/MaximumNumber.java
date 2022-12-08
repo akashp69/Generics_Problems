257ea4f808b0a50b8ede186f03c94bcd2a9b46c6
akashp69 <akashpakhare499@gmail.com>
2022-12-08 23:02:04 +0530

    Added: Program For Find the maximum float

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -2,30 +2,28 @@ package com.bridgelabz;
 
 public class MaximumNumber {
     /**
-     *This is Static method Used for comparing out of three integer which one is maximum number
+     *This is Static Method used for Find the maximum float value
      */
-    public static void testMaximumNumber(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    private static void testMaximum(Float a, Float b, Float c) {
+        Float max = a;
         if (b.compareTo(a) > 0) {
             max = b;
         }
         if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println("Maximum Number is:" + max);
+        System.out.println("Maximum num is:" + max);
     }
 
     /**
-     * This is Main Method is Used for calling the static method
-     * Using a,b and c Variables for three integers values
+     *This is Main Method is used for calling the static methods
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 25, b = 50, c =100;
-        System.out.println("a ="+a);
-        System.out.println("b ="+b);
-        System.out.println("c ="+c);
+        Float a = 15.2f, b= 15.8f, c = 15.6f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
-        testMaximumNumber (a,b,c);
-    }
+        testMaximum(a, b, c); 	}
 }
